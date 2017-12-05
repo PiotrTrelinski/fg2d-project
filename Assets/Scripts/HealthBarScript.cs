93fb93a81481ecaa000673140644880568623d29
Piotr Treliński <195078@edu.p.lodz.pl>
2017-12-05 23:51:06 +0100

    Add secondary HP bar to accentuate damage dealt

--- Assets/Scripts/HealthBarScript.cs
+++ Assets/Scripts/HealthBarScript.cs
@@ -8,6 +8,7 @@ public class HealthBarScript : MonoBehaviour {
     public CharacterControler character;
     public RectTransform parentBar;
     private RectTransform healthBar;
+    public RectTransform redHealthBar;
 
 	// Use this for initialization
 	void Start () {
@@ -16,6 +17,14 @@ public class HealthBarScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        if (redHealthBar.sizeDelta.x < healthBar.sizeDelta.x)
+        {
+            redHealthBar.sizeDelta = new Vector2(healthBar.sizeDelta.x, healthBar.sizeDelta.y);
+        }
         healthBar.sizeDelta = new Vector2((character.currentHealth*parentBar.sizeDelta.x)/100, healthBar.sizeDelta.y);
+        if((!character.isInHitStun||character.isKOd) && redHealthBar.sizeDelta.x > healthBar.sizeDelta.x)
+        {
+            redHealthBar.sizeDelta = new Vector2(redHealthBar.sizeDelta.x - parentBar.sizeDelta.x/100, healthBar.sizeDelta.y);
+        }
 	}
 }
