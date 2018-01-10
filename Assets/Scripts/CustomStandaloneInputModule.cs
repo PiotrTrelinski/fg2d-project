34b8c86137369b38f3daf6fa4e1f419952cc5d03
Piotr Treliński <195078@edu.p.lodz.pl>
2018-01-10 22:47:11 +0100

    Add extension of StandaloneInputModule, CustomStandaloneInputModule, which allows Y axis inversion to work in menu

---
+++ Assets/Scripts/CustomStandaloneInputModule.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CustomStandaloneInputModule : StandaloneInputModule {
+    public int PlayerNumber = 1;
+    protected override AxisEventData GetAxisEventData(float x, float y, float moveDeadZone)
+    {
+        return base.GetAxisEventData(x, y * PlayerPrefs.GetInt("InvertYAxisP"+PlayerNumber, 1), moveDeadZone);
+    }
+}
