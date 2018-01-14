3d47a1f6cffc54cffa17a2724dbd7c5c7c4fea6a
Piotr Treliński <195078@edu.p.lodz.pl>
2018-01-14 23:01:19 +0100

    Fix issue with joypad axes not working in character select

--- Assets/Scripts/CharacterSelect.cs
+++ Assets/Scripts/CharacterSelect.cs
@@ -18,6 +18,8 @@ public class CharacterSelect : MonoBehaviour
     private int p2CursorPosition = 7;
     private bool p1Selected = false;
     private bool p2Selected = false;
+    private bool p1Switchable = true;
+    private bool p2Switchable = true;
 
 	// Use this for initialization
 	void Awake ()
@@ -53,24 +55,38 @@ public class CharacterSelect : MonoBehaviour
             else
                 StartCoroutine("StartGame");
         }
-        if (Input.GetButtonDown("Horizontal P1") && !p1Selected)
+        if (Input.GetAxisRaw("Horizontal P1") != 0 && p1Switchable && !p1Selected)
         {
             p1CursorPosition += Input.GetAxisRaw("Horizontal P1") > 0 ? 1 : -1;
             p1CursorPosition = p1CursorPosition > 7 ? 0 : (p1CursorPosition < 0 ? 7 : p1CursorPosition);
             cursorP1.transform.position = new Vector3(colors[p1CursorPosition].transform.position.x - 15 / (scaler.referenceResolution.x / Screen.width), cursorP1.transform.position.y, cursorP1.transform.position.z);
             p1Renderer.material.color = colors[p1CursorPosition].GetComponent<Image>().color;
+            StartCoroutine("SwitchCooldownP1");
         }
-        if (Input.GetButtonDown("Horizontal P2") && !p2Selected)
+        if (Input.GetAxisRaw("Horizontal P2") != 0 && p2Switchable && !p2Selected)
         {
             p2CursorPosition += Input.GetAxisRaw("Horizontal P2") > 0 ? 1 : -1;
             p2CursorPosition = p2CursorPosition > 7 ? 0 : (p2CursorPosition < 0 ? 7 : p2CursorPosition);
             cursorP2.transform.position = new Vector3(colors[p2CursorPosition].transform.position.x + 15 / (scaler.referenceResolution.x / Screen.width), cursorP2.transform.position.y, cursorP2.transform.position.z);
             p2Renderer.material.color = colors[p2CursorPosition].GetComponent<Image>().color;
+            StartCoroutine("SwitchCooldownP2");
         }            
     }
-    IEnumerator StartGame()
+    private IEnumerator StartGame()
     {
         yield return new WaitForSeconds(3);
         SceneManager.LoadScene("scene");
+    }
+    private IEnumerator SwitchCooldownP1()
+    {
+        p1Switchable = false;
+        yield return new WaitForSeconds(0.2f);
+        p1Switchable = true;
+    }
+    private IEnumerator SwitchCooldownP2()
+    {
+        p2Switchable = false;
+        yield return new WaitForSeconds(0.2f);
+        p2Switchable = true;
     }
 }
